7a87bf592c800225d4da6cc1675c7cbe3e78ae16
hazel <hazel_faye@outlook.com>
2024-06-16 23:38:15 +0800

    logback

--- member/src/main/java/org/clone/config/MemberApplication.java
+++ member/src/main/java/org/clone/config/MemberApplication.java
@@ -1,15 +1,22 @@
 package org.clone.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("org.clone")
 public class MemberApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment evn = app.run(args).getEnvironment();
+        LOG.info("Start successfully.");
+        LOG.info("Addr: \thttp://127.0.0.1:{}", evn.getProperty("server.port"));
+
     }
 
 }
